bd037e5bb50122268a9cd18a38eed8032f055b03
Omar Abu Saada <rechard.the3@gmail.com>
2022-05-04 18:29:35 +0300

    fix(select-component): properly set select options

--- src/app/widgets/select/select.component.ts
+++ src/app/widgets/select/select.component.ts
@@ -1,21 +1,33 @@
-import { Component, ContentChildren, DoCheck, OnInit, AfterContentInit, ElementRef } from '@angular/core';
+import { Component, ContentChildren, DoCheck, OnInit, AfterContentInit, ElementRef, QueryList, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
-export class SelectComponent implements OnInit, AfterContentInit {
-  myOptions:any = [];
-  @ContentChildren('p') options: ElementRef[] =[];
+export class SelectComponent implements OnInit, AfterContentInit, OnDestroy {
+  myOptions: ElementRef<HTMLElement>[] = [];
+  @ContentChildren('p') options!: QueryList<ElementRef<HTMLElement>>;
   constructor() { }
-  ngAfterContentInit(): void {
-    this.myOptions =  this.options.map(ele => ele.nativeElement.innerHTML);
 
+  private _destroy$ = new Subject();
+  ngAfterContentInit(): void {
+    this.myOptions = Array.from(this.options);
+    debugger;
+    this.options.changes.pipe(
+      takeUntil(this._destroy$)
+    ).subscribe((newOptions: ElementRef<HTMLElement>[]) => {
+      debugger;
+      this.myOptions = newOptions;
+    });
   }
 
   ngOnInit(): void {
   }
 
-
+  ngOnDestroy(): void {
+    this._destroy$.next();
+  }
 }
