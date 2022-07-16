cf78616c94a288fb537b6baa12e3039a6ffe5304
Wissam-Feh-Al-Shohof <wissam.fehmi@gmail.com>
2022-07-16 21:18:18 +0300

    inintializing core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { VisibleDirective } from './visible/visible.directive';
+
+
+@NgModule({
+  declarations: [VisibleDirective],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
